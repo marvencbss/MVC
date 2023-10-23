17d60f987619aa8bfefa6b2602e2c486baccc92f
marvencbss~ <marvencbs12@gmail.com~>
2023-10-23 20:30:14 -0300

    .

--- console.js
+++ console.js
@@ -1,3 +1,40 @@
 const rl = require('readline-sync');
-const nome = rl.question('Qual seu nome? ')
-console.log(`Bem vindo ${nome}.`);
+
+class Pessoa {
+    Nome
+    Email
+    Idade
+
+    get nome() {
+        return this.Nome;
+    }
+
+    set nome(novoNome) {
+            this.Nome = novoNome;
+    }
+
+    get email() {
+        return this.Email;
+    }
+
+    set email(novoEmail) {
+        this.Email = novoEmail;
+    }
+
+    get idade() {
+        return this.Idade;
+    }
+
+    set idade(novoIdade) {
+        this.Idade = novoIdade;
+    }
+}
+
+let novaPessoa = new Pessoa();
+
+const valorNome = rl.question('Qual seu nome? ');
+const valorEmail = rl.question('Qual seu email? ');
+const valorIdade = rl.question('Qual sua idade? ');
+
+console.log(`Bem vindo(a) ${valorNome}! seu email é ${valorEmail} e você tem ${valorIdade} anos.`);
+console.table(novaPessoa);
